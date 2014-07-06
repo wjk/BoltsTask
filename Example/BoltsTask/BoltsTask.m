dcb6c156c904c63b287878d03e2adaf8c31dc5d5
William Kent <wjk011@gmail.com>
2014-07-06 17:53:02 -0400

    Update example pod dependencies

---
+++ Example/BoltsTask/BoltsTask.m
@@ -0,0 +1,39 @@
+//
+//  BoltsTask.m
+//  BoltsTask
+//
+//  Created by William Kent on 7/6/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BoltsTask : XCTestCase
+
+@end
+
+@implementation BoltsTask
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Example/Pods/BuildHeaders/BoltsTask/BFExecutor.h
@@ -0,0 +1,1 @@
+../../../../Pod/Classes/BFExecutor.h
